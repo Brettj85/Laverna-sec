a033209ca435485fbe385e894b4a79b260ea1f5e
brett <brettj85@gmail.com>
2019-12-16 19:36:22 -0600

    completed nav elements and structure collapse

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,20 @@
 document.getElementById("hamburg").addEventListener("click", function(){
     var hamburger = document.getElementById("hamburg");
+    var content = document.getElementById("contentContainer");
     hamburger.classList.toggle("is-active");
+    content.classList.toggle("open");
+    content.classList.toggle("closed");
+    if(content.classList.contains("open")){
+    zIndex(500);
+    }
+    else{
+        var up = document.getElementById("up");
+        var down = document.getElementById("down");
+        up.classList.toggle("visible");
+        up.classList.toggle("invisible");
+        down.classList.toggle("visible");
+        down.classList.toggle("invisible");
+    }
 });
 
 document.getElementById("up").addEventListener("click", function(){
@@ -10,3 +24,20 @@ document.getElementById("up").addEventListener("click", function(){
 document.getElementById("down").addEventListener("click", function(){
     var hamburger = document.getElementById("down");
 });
+
+async function zIndex(time){    
+    var result = await sleep(time);
+}
+
+function sleep(time) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+        var up = document.getElementById("up");
+        var down = document.getElementById("down");
+        up.classList.toggle("visible");
+        up.classList.toggle("invisible");
+        down.classList.toggle("visible");
+        down.classList.toggle("invisible");
+    }, time);
+  });
+}
