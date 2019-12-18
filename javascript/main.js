9e335d2fac7b48e8cff8e0860eb66f7c7ad39130
brett <brettj85@gmail.com>
2019-12-17 19:49:42 -0600

    added header added/modified javascript to only show scroll buttons and hamburger when nessacary

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,6 @@
-document.getElementById("hamburg").addEventListener("click", function(){
+document.getElementById("hamburg").addEventListener("click", hamburgerAction);
+
+function hamburgerAction(){
     var hamburger = document.getElementById("hamburg");
     var content = document.getElementById("contentContainer");
     hamburger.classList.toggle("is-active");
@@ -15,7 +17,7 @@ document.getElementById("hamburg").addEventListener("click", function(){
         down.classList.toggle("visible");
         down.classList.toggle("invisible");
     }
-});
+}
 
 document.getElementById("up").addEventListener("click", function(){
     var h = document.documentElement.clientHeight;
@@ -46,3 +48,53 @@ function sleep(time) {
   });
 }
 
+var visibleY = function(el){
+  var rect = el.getBoundingClientRect(), top = rect.top, height = rect.height, 
+    el = el.parentNode;
+  do {
+    rect = el.getBoundingClientRect();
+    if (top <= rect.bottom === false) return false;
+    // Check if the element is out of view due to a container scrolling
+    if ((top + height) <= rect.top) return false
+    el = el.parentNode;
+  } while (el != document.body);
+  // Check its within the document viewport
+  return top <= document.documentElement.clientHeight;
+};
+
+function attachEvent(element, event, callbackFunction) {
+    if (element.addEventListener) {
+        element.addEventListener(event, callbackFunction, false);
+    } else if (element.attachEvent) {
+        element.attachEvent('on' + event, callbackFunction);
+    }
+};
+
+var update = function(){
+    if(visibleY(document.getElementById('header'))){
+        up.classList.remove("visible");
+        up.classList.add("invisible");
+        hamburg.classList.remove("hamburger-position");
+        hamburg.classList.add("invisible");
+    }
+    else
+    {
+        up.classList.add("visible");
+        up.classList.remove("invisible");
+        hamburg.classList.add("hamburger-position");
+        hamburg.classList.remove("invisible");
+    }
+    if(visibleY(document.getElementById('checkBottom'))){
+        down.classList.remove("visible");
+        down.classList.add("invisible");
+    }
+    else
+    {
+        down.classList.add("visible");
+        down.classList.remove("invisible");
+    }
+};
+
+attachEvent(document.getElementById('mainContent'), "scroll", update);
+attachEvent(window, "resize", update);
+update();
